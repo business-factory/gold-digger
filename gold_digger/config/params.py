c3f0cb0d01565ff12fa7b6e4b94d2626034f051d
Martin Veselovský <martin.veselovskyy@gmail.com>
2016-02-15 14:33:45 +0100

    Provider urls are specified in provider classes.

--- gold_digger/config/params.py
+++ gold_digger/config/params.py
@@ -11,9 +11,5 @@ DEFAULT_CONFIG_PARAMS = {
         "host": "127.0.0.1",
         "port": "5433",
         "dialect": "postgres"
-    },
-    "data_providers": {
-        "currency_layer": "http://www.apilayer.net/api/live?access_key=8497c277171dfc3ad271f1ccb733a6a8",
-        "grandtrunk": "http://currencies.apps.grandtrunk.net/"
     }
 }
