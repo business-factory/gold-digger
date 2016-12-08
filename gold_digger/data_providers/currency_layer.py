9d414adfa10995c08a38e2413516b52ab752a124
Martin Veselovsky <martin.veselovskyy@gmail.com>
2016-12-08 12:47:39 +0100

    CurrencyLayer provider - new access key + check errors

--- gold_digger/data_providers/currency_layer.py
+++ gold_digger/data_providers/currency_layer.py
@@ -9,7 +9,11 @@ class CurrencyLayer(Provider):
     Real-time service with free plan for 1000 requests per month.
     Implicit base currency is USD.
     """
-    BASE_URL = "http://www.apilayer.net/api/live?access_key=8497c277171dfc3ad271f1ccb733a6a8"
+    ACCESS_KEYS = [
+        "8497c277171dfc3ad271f1ccb733a6a8",  # martin.veselovsky@b.cz
+        "aa94a1ce1a597051b8bc3c117f6b8f25",  # martin.veselovsky@roihunter.com
+    ]
+    BASE_URL = "http://www.apilayer.net/api/live?access_key=%s" % ACCESS_KEYS[1]
     BASE_CURRENCY = "USD"
     name = "currency_layer"
 
@@ -18,7 +22,16 @@ class CurrencyLayer(Provider):
         self.logger.debug("Requesting CurrencyLayer for %s (%s)", currency, date_str, extra={"currency": currency, "date": date_str})
 
         response = self._get("{url}&date={date}&currencies={currencies}".format(url=self.BASE_URL, date=date_str, currencies=currency))
-        records = response.json().get("quotes", {}) if response else {}
+        if not response:
+            self.logger.warning("CurrencyLayer error. Status: %s", response.status_code)
+            return None
+
+        response = response.json()
+        if response and response.get("success") is False:
+            self.logger.warning("CurrencyLayer unsuccessful request. Error: %s", response.get("error", {}).get("info"))
+            return None
+
+        records = response.get("quotes", {}) if response else {}
         value = records.get("%s%s" % (self.BASE_CURRENCY, currency))
         return self._to_decimal(value, currency) if value is not None else None
 
