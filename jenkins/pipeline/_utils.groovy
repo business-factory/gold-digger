5778e537783206db3b480bb4a5c996c1568cb7cf
harpener <harpener@gmail.com>
2018-04-27 09:40:15 +0200

    Add releaser URL to slack notification

--- jenkins/pipeline/_utils.groovy
+++ jenkins/pipeline/_utils.groovy
@@ -1,6 +1,6 @@
 def sendSlackNotification(String branch_name, String color) {
     withCredentials([string(credentialsId: 'slack-bot-token', variable: 'slack_token')]) {
-        message = "Gold Digger $branch_name is deployed."
+        message = "Gold Digger $branch_name is deployed. Please release changes at https://py.b.cz/helpers/releaser/gold-digger/"
         slackSend channel: 'python-alerts', message: message, color: "$color", token: slack_token, botUser: true
     }
 }
