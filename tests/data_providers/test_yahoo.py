228479e29a7230e7a66c41b982b8ce0849ea435f
Jozef Harag <32jojo32@gmail.com>
2018-11-09 09:05:21 +0100

    Adds tests, #26540

--- tests/data_providers/test_yahoo.py
+++ tests/data_providers/test_yahoo.py
@@ -8,43 +8,155 @@ from requests import Response
 
 YAHOO_RESPONSE = b"""
 {
-    "list": {
-        "meta": {
-            "type": "resource-list",
-            "start": 0,
-            "count": 188
-        },
-        "resources": [
-            {
-                "resource": {
-                    "classname": "Quote",
-                    "fields": {
-                        "vname": "USD/EUR",
-                        "price": "0.861800",
-                        "symbol": "EUR=X",
-                        "ts": "1510220668",
-                        "type": "currency",
-                        "utctime": "2017-11-09T09:44:28+0000",
-                        "volume": "0"
-                    }
-                }
-            },
-            {
-                "resource": {
-                    "classname": "Quote",
-                    "fields": {
-                        "name": "USD/CZK",
-                        "price": "21.988899",
-                        "symbol": "CZK=X",
-                        "ts": "1510220626",
-                        "type": "currency",
-                        "utctime": "2017-11-09T09:43:46+0000",
-                        "volume": "0"
-                    }
-                }
-            }
-        ]
-    }
+   "spark":{
+      "result":[
+         {
+            "symbol":"NOK=X",
+            "response":[
+               {
+                  "meta":{
+                     "currency":"NOK",
+                     "symbol":"NOK=X",
+                     "exchangeName":"CCY",
+                     "instrumentType":"CURRENCY",
+                     "firstTradeDate":995238000,
+                     "gmtoffset":0,
+                     "timezone":"GMT",
+                     "exchangeTimezoneName":"Europe/London",
+                     "chartPreviousClose":8.3756,
+                     "priceHint":5,
+                     "currentTradingPeriod":{
+                        "pre":{
+                           "timezone":"GMT",
+                           "start":1541721600,
+                           "end":1541721600,
+                           "gmtoffset":0
+                        },
+                        "regular":{
+                           "timezone":"GMT",
+                           "start":1541721600,
+                           "end":1541807940,
+                           "gmtoffset":0
+                        },
+                        "post":{
+                           "timezone":"GMT",
+                           "start":1541807940,
+                           "end":1541807940,
+                           "gmtoffset":0
+                        }
+                     },
+                     "dataGranularity":"1d",
+                     "validRanges":[
+                        "1d",
+                        "5d",
+                        "1mo",
+                        "3mo",
+                        "6mo",
+                        "1y",
+                        "2y",
+                        "5y",
+                        "10y",
+                        "ytd",
+                        "max"
+                     ]
+                  },
+                  "timestamp":[
+                     1541759519
+                  ],
+                  "indicators":{
+                     "quote":[
+                        {
+                           "close":[
+                              8.44
+                           ]
+                        }
+                     ],
+                     "adjclose":[
+                        {
+                           "adjclose":[
+                              8.44
+                           ]
+                        }
+                     ]
+                  }
+               }
+            ]
+         },
+         {
+            "symbol":"NIO=X",
+            "response":[
+               {
+                  "meta":{
+                     "currency":"NIO",
+                     "symbol":"NIO=X",
+                     "exchangeName":"CCY",
+                     "instrumentType":"CURRENCY",
+                     "firstTradeDate":1070236800,
+                     "gmtoffset":0,
+                     "timezone":"GMT",
+                     "exchangeTimezoneName":"Europe/London",
+                     "chartPreviousClose":31.96,
+                     "priceHint":3,
+                     "currentTradingPeriod":{
+                        "pre":{
+                           "timezone":"GMT",
+                           "start":1541721600,
+                           "end":1541721600,
+                           "gmtoffset":0
+                        },
+                        "regular":{
+                           "timezone":"GMT",
+                           "start":1541721600,
+                           "end":1541807940,
+                           "gmtoffset":0
+                        },
+                        "post":{
+                           "timezone":"GMT",
+                           "start":1541807940,
+                           "end":1541807940,
+                           "gmtoffset":0
+                        }
+                     },
+                     "dataGranularity":"1d",
+                     "validRanges":[
+                        "1d",
+                        "5d",
+                        "1mo",
+                        "3mo",
+                        "6mo",
+                        "1y",
+                        "2y",
+                        "5y",
+                        "10y",
+                        "ytd",
+                        "max"
+                     ]
+                  },
+                  "timestamp":[
+                     1541743835
+                  ],
+                  "indicators":{
+                     "quote":[
+                        {
+                           "close":[
+                              32.106
+                           ]
+                        }
+                     ],
+                     "adjclose":[
+                        {
+                           "adjclose":[
+                              32.106
+                           ]
+                        }
+                     ]
+                  }
+               }
+            ]
+         }
+      ],
+      "error":null
+   }
 }
 """
 
@@ -55,7 +167,14 @@ def test_yahoo_get_by_date(yahoo):
     sample._content = YAHOO_RESPONSE
     yahoo._get = lambda *a, **kw: sample
 
-    assert yahoo.get_by_date(date.today(), "EUR") == Decimal("0.861800")
+    assert yahoo.get_by_date(date.today(), "NOK") == Decimal("8.44")
+
+
+def test_yahoo_get_by_date__unsupported_currency(yahoo):
+    sample = Response()
+    sample.status_code = 404
+    sample._content = "404 Not Found"
+    yahoo._get = lambda *a, **kw: sample
     assert yahoo.get_by_date(date.today(), "XXX") is None  # unsupported currency
 
 
@@ -63,11 +182,13 @@ def test_yahoo_get_all_by_date(yahoo):
     sample = Response()
     sample.status_code = 200
     sample._content = YAHOO_RESPONSE
-    yahoo._get = lambda *a, **kw: sample
 
-    rates = yahoo.get_all_by_date(date.today(), {"EUR", "CZK", "EEK"})
+    yahoo._get = lambda url, **kw: sample
+
+    rates = yahoo.get_all_by_date(date.today(), {"NOK", "NIO", "EUR"})
 
     assert rates == {
-        "EUR": Decimal("0.861800"),
-        "CZK": Decimal("21.988899"),
+        "NOK": Decimal("8.44"),
+        "NIO": Decimal("32.106"),
+        "EUR": None
     }
