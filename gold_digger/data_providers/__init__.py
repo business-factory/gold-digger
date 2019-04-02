2cd34328ab952dd3598615f548eca7a7069f1a60
Jozef Harag <32jojo32@gmail.com>
2019-04-02 15:10:08 +0200

    Removes Unicredit provider, #28279

--- gold_digger/data_providers/__init__.py
+++ gold_digger/data_providers/__init__.py
@@ -4,5 +4,4 @@ from ._provider import Provider
 from .currency_layer import CurrencyLayer
 from .fixer import Fixer
 from .grandtrunk import GrandTrunk
-from .unicredit import Unicredit
 from .yahoo import Yahoo
--- tests/data_providers/conftest.py
+++ tests/data_providers/conftest.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from gold_digger.data_providers import Fixer, Unicredit, Yahoo
+from gold_digger.data_providers import Fixer, Yahoo
 
 
 @pytest.fixture
@@ -13,8 +13,3 @@ def yahoo(base_currency, currencies):
 @pytest.fixture
 def fixer(base_currency, logger):
     return Fixer("simple_access_key", logger, base_currency)
-
-
-@pytest.fixture
-def unicredit(base_currency, currencies):
-    return Unicredit(base_currency)
