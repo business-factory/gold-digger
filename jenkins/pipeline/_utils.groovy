b98a49e1b227e19c0d37e31a926dda9f638ac870
harpener <harpener@gmail.com>
2019-07-22 18:01:36 +0200

    Add automatic release

--- jenkins/pipeline/_utils.groovy
+++ jenkins/pipeline/_utils.groovy
@@ -1,6 +1,5 @@
-def sendSlackNotification(String branch_name, String color) {
+def sendSlackNotification(String color, String message) {
     withCredentials([string(credentialsId: 'slack-bot-token', variable: 'slack_token')]) {
-        message = "Gold Digger $branch_name is deployed. Please release changes at https://py.b.cz/helpers/releaser/gold-digger/"
         slackSend channel: 'python-alerts', message: message, color: "$color", token: slack_token, botUser: true
     }
 }
--- jenkins/pipeline/deploy-master.groovy
+++ jenkins/pipeline/deploy-master.groovy
@@ -1,4 +1,4 @@
-def build, utils
+def build, github, utils
 
 pipeline {
     agent {
@@ -11,9 +11,14 @@ pipeline {
 
     parameters {
         booleanParam(
-            name: 'SEND_SLACK_NOTIFICATION',
-            defaultValue: true,
-            description: 'Send notification about deploy to Slack.'
+            name: "FEATURE_RELEASE",
+            defaultValue: false,
+            description: "Whether feature or bug fix should be released."
+        )
+        string(
+            name: "RELEASE_VERSION",
+            defaultValue: "",
+            description: "Specify release version number. (FEATURE_RELEASE will be ignored)"
         )
     }
 
@@ -27,6 +32,7 @@ pipeline {
                 script {
                     def rootDir = pwd()
                     build = load "${rootDir}/jenkins/pipeline/_build.groovy"
+                    github = load "${rootDir}/jenkins/pipeline/_github.groovy"
                     utils = load "${rootDir}/jenkins/pipeline/_utils.groovy"
                 }
             }
@@ -67,15 +73,70 @@ pipeline {
             }
         }
 
-        stage('Send Slack notification') {
-            when {
-                expression {
-                    return params.SEND_SLACK_NOTIFICATION
-                }
-            }
+        stage("Do GitHub release") {
             steps {
                 script {
-                    utils.sendSlackNotification(env.BRANCH_NAME, '#0E8A16')
+                    def doRelease = github.getReleasePreview()
+                    def currentMaster = github.getLatestRelease()
+
+                    try {
+                        if (doRelease) {
+                            // Parse the version number and prepare the new version
+                            String newMaster = null
+
+                            // Check if version was specified manually
+                            if (params.RELEASE_VERSION != "") {
+                                // At least basic validation has to be done first
+                                def versionRegex = /^\d+\.\d+\.\d+$/
+                                def versionMatcher = params.RELEASE_VERSION =~ versionRegex
+                                if (!versionMatcher.matches()) {
+                                    error("Provided version is badly formatted. Enter a valid version or leave it empty for release to decide automatically.")
+                                } else {
+                                    newMaster = params.RELEASE_VERSION
+                                }
+
+                            } else if (params.FEATURE_RELEASE) {
+                                def currentMasterMajorReleaseString = currentMaster.substring(0, currentMaster.indexOf("."))
+                                def currentMasterMinorReleaseString = currentMaster.substring(currentMaster.indexOf(".") + 1, currentMaster.lastIndexOf("."))
+                                int currentMasterMinorReleaseNumber = currentMasterMinorReleaseString.toInteger()
+                                int newMasterMinorReleaseNumber = currentMasterMinorReleaseNumber + 1
+                                newMaster = currentMasterMajorReleaseString + "." + newMasterMinorReleaseNumber.toString() + ".0"
+
+                            } else {
+                                // First number is the major release (hardly ever changes),
+                                // middle is minor (changes on feature master deploys),
+                                // last is build (increments on every build)
+                                def currentMasterMajorAndMinorReleaseString = currentMaster.substring(0, currentMaster.lastIndexOf(".") + 1)
+                                def currentMasterBuildString = currentMaster.substring(currentMaster.lastIndexOf(".") + 1)
+                                int currentMasterBuildNumber = currentMasterBuildString.toInteger()
+                                int newMasterBuildNumber = currentMasterBuildNumber + 1
+                                newMaster = currentMasterMajorAndMinorReleaseString + newMasterBuildNumber.toString()
+                            }
+
+                            println(newMaster)
+
+                            def body = "version=${newMaster}"
+                            withCredentials([string(credentialsId: "releaser-authorization", variable: "releaserAuthorization")]) {
+                                httpRequest(
+                                    customHeaders: [[name: "Authorization", value: releaserAuthorization],
+                                    [name: "Content-Type", value: "application/x-www-form-urlencoded"]],
+                                    ignoreSslErrors: true,
+                                    httpMode: "POST",
+                                    requestBody: body,
+                                    url: """https://py.b.cz/helpers/releaser/gold-digger/release/master/$currentMaster...master"""
+                                )
+                            }
+
+                        } else {
+                            println("Not doing the release, because there were no commits to release.")
+                        }
+                    } catch (err) {
+                        utils.sendSlackNotification(
+                            "#FF0000",
+                            "Gold Digger ${env.BRANCH_NAME} release failed. Please release changes manually at https://py.b.cz/helpers/releaser/gold-digger/"
+                        )
+                        println("GitHub release failed. Error: " + err)
+                    }
                 }
             }
         }
