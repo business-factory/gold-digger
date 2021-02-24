38a325b38136174a20752850481cc7decd96fa0f
harpener <harpener@gmail.com>
2021-02-24 12:57:47 +0100

    RH-30599: Cleanup pipelines

--- jenkins/pipeline/_utils.groovy
+++ jenkins/pipeline/_utils.groovy
@@ -1,5 +1,5 @@
 def sendSlackNotification(String color, String message) {
-    slackSend channel: 'python-alerts', message: message, color: "$color", tokenCredentialId: "slack-bot-token", botUser: true
+    slackSend channel: "python-alerts", message: message, color: "$color", tokenCredentialId: "slack-bot-token", botUser: true
 }
 
 return this
